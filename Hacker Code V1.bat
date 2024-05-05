c6ef30d91d2c99ba13bec18d21dd21085527f939
RTGAnormalDev <168943527+RTGAnormalDev@users.noreply.github.com>
2024-05-04 21:12:44 -0400

    Hacker Code V1
    Looks like you are cracking a security thing I really do not know
    Made in notepad so no Viruses or rats its really laggy when you start it so don't use this on a really bad pc

---
+++ Hacker Code V1.bat
@@ -0,0 +1,30 @@
+
+@echo off
+:a
+color 02
+title Hacking Data Steals
+echo %random% %random% %random% %random% %random% %random%
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random%
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% 
+
+goto a
